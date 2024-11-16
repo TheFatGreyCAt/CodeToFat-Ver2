54c78af74f8b8b5a340bffb66ef3327a70b9f401
TheFatGreyCat <nhtung2705@gmail.com>
2024-11-16 17:24:22 +0700

    16/11/2024

--- Code/Project/Web Tuyen Sinh/JS/script.js
+++ Code/Project/Web Tuyen Sinh/JS/script.js
@@ -1,5 +1,26 @@
 let profile = document.querySelector('.profile');
 
-document.querySelector('#user-btn').onclick = () =>{
-   profile.classList.toggle('active');
+document.querySelector('#user-btn').onclick = () => {
+    profile.classList.toggle('active');
 }
+
+let slideIndex = 0;
+const slideShow = document.querySelector('.stg-box');
+const slides = document.querySelectorAll('.slide-show');
+
+const showSlide = (index) => {
+    let slideWidth = slides[0].offsetWidth;
+    slideShow.style.transform = 'translateX(' + (-slideWidth * index) + 'px)';
+}
+
+document.querySelector('#prev-slide').onclick = () => {
+    slideIndex = (slideIndex > 0) ? slideIndex - 1 : slides.length - 1;
+    showSlide(slideIndex);
+}
+
+document.querySelector('#next-slide').onclick = () => {
+    slideIndex = (slideIndex < slides.length - 1) ? slideIndex + 1 : 0;
+    showSlide(slideIndex);
+}
+
+showSlide(slideIndex);
