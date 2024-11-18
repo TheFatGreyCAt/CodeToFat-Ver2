894ba8b7583c4166b0166786b01b1492662f6bb8
TheFatGreyCat <zoptemz22072005@gmail.com>
2024-11-18 04:17:24 +0000

    begin 18/11

--- Code/CPP/FatCatForm.cpp
+++ Code/CPP/FatCatForm.cpp
@@ -14,9 +14,103 @@
 using namespace std;
 ld a[nmax],b,c,m,n,d,t;
 
+// string checkName(string name)
+// {
+//     string stg = "Anh";
+//     return name.find(stg);
+// }
+
+int DoM(int m, int y)
+{
+    int res = 31;
+    if(m == 2){
+        if(y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) res = 29;
+        else res = 28;
+    }
+    else if(m == 4 || m == 6 || m == 9 || m == 11) res = 30;
+    return res;
+}
+
+struct TDate
+{
+    int d,m,y;
+};
+
+int ageCal(TDate birth)
+{
+    TDate cur(17,11,2024);
+    int age = cur.y - birth.y;
+    if (cur.m < birth.m || (cur.m == birth.m && cur.d < birth.d)) age--;
+    return age;
+}
+
+class Person
+{
+    private:
+        string name;
+        TDate birth;
+        string country;
+    public:
+        Person() : name(), birth(), country() {} 
+        Person(string name, TDate birth, string country ) : name(name), birth(birth), country(country){}
+        Person(Person &p) : name(p.name), birth(p.birth), country(p.country){}
+        TDate getBirth()
+        {
+            return birth;
+        }
+        int getAge()
+        {
+            return ageCal(birth);
+        }
+        void input()
+        {
+            cin.ignore();
+            cout << "Name: " << "\n";  getline(cin, name);
+            cout << "Birth: (d m y)" << "\n"; cin >> birth.d >> birth.m >> birth.y;
+            cin.ignore();
+            cout << "Country: " << "\n"; getline(cin, country);
+        }
+        void output()
+        {
+            cout << "Name: " << name << endl;
+            cout << "Birth: " << birth.d << "/" << birth.m << "/" << birth.y << endl;
+            cout << "Country: " << country << endl;
+        }
+};
+
 void fatcat()
 {
-    
+    int n;
+    cin >> n;
+    Person ps[50];
+    int ages;
+    for(int i = 0; i < n ; i++)
+    {
+        cout << "Person " << i + 1 << endl;
+        ps[i].input();
+        a[i] = ps[i].getAge();
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for( int j = i + 1 ; j  < n ; j++)
+        {
+            if(ages[i] > ages[j])
+            {
+                swap(ages[i],ages[j]);
+                Person tmp = ps[i];
+                ps[i] = ps[j];
+                ps[j] = tmp;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << "Person " << i + 1 << endl;
+        ps[i].output();
+    }
+
 }
 
 int main()
